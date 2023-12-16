326e272ac8381a005fec127cafcde427ba5de94c
Sumit-9900 <paulsumit9900@gmail.com>
2023-12-16 10:17:09 +0530

    hello

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:calculator_1/screen/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: const MainScreen(),
+    );
+  }
+}
